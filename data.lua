a97e6c1b038247e98099d2107b66d053454b0e6c
EpicFail1403 <dick.templar@gmail.com>
2017-09-26 10:42:41 +0800

    Made productivity modules applicable to the recipe
    As 'similar to the base games coal liquefaction'

--- data.lua
+++ data.lua
@@ -2,3 +2,9 @@
 
 require("prototypes.recipe")
 require("prototypes.technology")
+
+for k, v in pairs(data.raw.module) do
+  if v.name:find("productivity%-module") and v.limitation then
+    table.insert(v.limitation, "hydrothermal-liquefaction")
+  end
+end
